8e4647a2a9f1703269a0483d9b63e336b27597d6
jiangmuyuan <jiangmuyuan@sogou-inc.com>
2019-11-17 13:59:45 +0800

    提交

--- src/index.js
+++ src/index.js
@@ -23,19 +23,7 @@ import {
 import './public/css/base.css'
 import './public/css/patch.css'
 import './public/css/animate.css'
-import 'element-ui/lib/theme-chalk/icon.css'
-import 'element-ui/lib/theme-chalk/date-picker.css'
-import 'element-ui/lib/theme-chalk/table.css'
-import 'element-ui/lib/theme-chalk/table-column.css'
-import 'element-ui/lib/theme-chalk/select.css'
-import 'element-ui/lib/theme-chalk/option.css'
-import 'element-ui/lib/theme-chalk/input.css'
-import 'element-ui/lib/theme-chalk/button.css'
-import 'element-ui/lib/theme-chalk/radio.css'
-import 'element-ui/lib/theme-chalk/pagination.css'
-import 'element-ui/lib/theme-chalk/dialog.css'
-import 'element-ui/lib/theme-chalk/form.css'
-import 'element-ui/lib/theme-chalk/form-item.css'
+
 
 import axios from 'axios'
 window.axios = axios
@@ -57,16 +45,12 @@ Vue.use(FormItem)
 
 const originalPush = VueRouter.prototype.push
 VueRouter.prototype.push = function push(location) {
-    return originalPush.call(this, location).catch(function(err){
-        return err
-    })
+    return originalPush.call(this, location).catch(err => err)
 }
 
 var vm = new Vue({
     el: '#app',
-    render: function(c) {
-        return c(app)
-    },
-    router: router,
-    store: store,
+    render: c => c(app),
+    router,
+    store,
 })
